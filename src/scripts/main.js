40ac8cf6f9a080cf77f355b0503301ff11798daa
eldar mutaev <eldarmutaev@inbox.ru>
2022-10-08 18:33:06 +0300

    desktop finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,4 +9,12 @@ $(function () {
     slidesToScroll: 3,
   });
 
+  $('.attendance__slider').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+
 })
