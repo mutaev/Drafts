5330976f15043bc106d1a35d80685c1aed546e53
eldar mutaev <eldarmutaev@inbox.ru>
2022-10-09 17:24:55 +0300

    сделал адаптив почти

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,25 @@ $(function () {
   $('.news__slider').slick({
     dots: true,
     arrows: false,
-    autoplay: true,
+    autoplay: false,
     slidesToShow: 3,
     slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1010,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 796,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
 
   $('.attendance__slider').slick({
@@ -17,4 +33,10 @@ $(function () {
     slidesToScroll: 1,
   });
 
+
+  $('.menu-burger__header').click(function(){
+    $('.menu-burger__header').toggleClass('open-menu');
+    $('.header__nav').toggleClass('open-menu');
+    $('body').toggleClass('fixed-page');
+});
 })
