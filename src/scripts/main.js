3e996eee1784190c2c514d1466db57135cbd658a
eldar mutaev <eldarmutaev@inbox.ru>
2022-10-10 00:39:02 +0300

    почти готово

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,11 @@
 $(function () { 
-
-  //slider main
   $('.news__slider').slick({
     dots: true,
     arrows: false,
-    autoplay: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1010,
@@ -16,7 +15,7 @@ $(function () {
         }
       },
       {
-        breakpoint: 796,
+        breakpoint: 767,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -31,12 +30,29 @@ $(function () {
     autoplay: true,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplaySpeed: 6000,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 980,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
-
 
   $('.menu-burger__header').click(function(){
     $('.menu-burger__header').toggleClass('open-menu');
     $('.header__nav').toggleClass('open-menu');
     $('body').toggleClass('fixed-page');
-});
+  });
+
 })
